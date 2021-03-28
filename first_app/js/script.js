41b2bfd13c473b12c0dfac80e33d1f04d2c2a86a
Iurij <amourphys@gmail.com>
2021-03-28 19:47:16 +0300

    change script.js

--- first_app/js/script.js
+++ first_app/js/script.js
@@ -17,3 +17,59 @@ const a = prompt('Один из последних просмотренных ф
       personalMovieDB.movies[c] = d;
 
       console.log(personalMovieDB);
+
+// условные операторы
+
+let num = 50;
+
+    if(num < 49) {
+        console.log('Error');
+    } else if (num > 100) {
+        console.log('Much many');
+    } else {
+        console.log('Ok');
+    }
+
+    (num === 50) ? console.log('Ok') : console.log('Error');
+
+    switch (num) {
+        case 49:
+            console.log('Not right');
+            break;
+        case 100:
+            console.log('Not right');
+            break;
+        case 50:
+            console.log('All right');
+            break;
+        default:
+            console.log('Another choice');
+            break;      
+    }
+
+// Циклы
+
+// While
+
+while (num <= 55) {
+    console.log(num);
+    num++;
+}
+
+// Do While
+
+do {
+    console.log(num);
+    num++;
+}
+while (num < 55);
+
+// For
+
+for (let i = 1; i < 10; i++) {
+    if (i === 6) {
+        //break;
+        continue;
+    }
+    console.log(i);
+}
