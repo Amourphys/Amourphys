bd66394f85fd89ad9f4e06fbc38d8aa54252a922
Iurij <amourphys@gmail.com>
2021-03-28 23:14:38 +0300

    change function.js

--- first_app/js/function.js
+++ first_app/js/function.js
@@ -1,2 +1,44 @@
 'use strict';
 
+// Function declaration
+
+let num = 20;
+
+function showFirstMessage(text) {
+    console.log(text);
+    let num = 10;
+    console.log(num);
+}
+
+showFirstMessage('Hello');
+console.log(num);
+
+function calc(a, b) {
+    return (a + b);
+}
+
+console.log(calc(4, 3));
+
+function ret() {
+    let num = 50;
+
+    return num;
+}
+
+const anotherNum = ret();
+console.log(anotherNum);
+
+// Function expression
+
+const logger = function() {
+    console.log('Hello');
+};
+
+logger();
+
+// Стрелочные функции
+
+const calcul = (a, b) => {
+    console.log('3');
+    return a + b;
+};
